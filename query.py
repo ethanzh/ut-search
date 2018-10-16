a22351b1820f5b28a640758ebef1d7a85f60ffb9
Ethan Houston <ethan.houston@gmail.com>
2018-10-16 16:25:15 -0500

    Add terminator when # of requests is exceeded

--- query.py
+++ query.py
@@ -3,6 +3,7 @@ import re
 import requests
 import pandas as pd
 import random
+import sys
 
 # Used to slice list after shuffling a list of several thousand names
 NUM_NAMES = 10
@@ -14,6 +15,12 @@ BASE_QUERY_CS_URL = "https://directory.utexas.edu/index.php?q=%28%26%28cn%3D{0}*
 
 def make_query(name):
     return_html = requests.get(BASE_QUERY_CS_URL.format(name)).text
+
+    # If we've exceeded # of requests, terminate program
+    if "You have exceeded the number of allowed requests" in return_html:
+        print("Exceeded number of allowed requests")
+        sys.exit()
+
     soup = BeautifulSoup(return_html, 'html.parser')
     raw_names = str(soup.find_all("a", href=lambda href: href and "index.php?q=%28%26%28cn%3D" in href))
     return master_clean(raw_names)
@@ -50,9 +57,9 @@ counter = 0
 # TODO: @Stefan make this more Pythonic
 for individual_name in shortened_list:
     returned_names = make_query(individual_name)
-
     counter += 1
-    print("Completed " + str(counter) + " of  " +  str(NUM_NAMES) + " trials")
+
+    print("Completed " + str(counter) + " of " + str(NUM_NAMES) + " trials")
 
     # TODO: add check for 'vCard', we also need to figure out what happens when no names are returned
     for single_name in returned_names:
