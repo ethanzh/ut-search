a5cdbefc56c14a828b357a15f883637008a21034
Ethan Houston <ethan.houston@gmail.com>
2018-10-16 15:58:45 -0500

    Add lists of 5000+ male and female names

--- query.py
+++ query.py
@@ -2,6 +2,10 @@ from bs4 import BeautifulSoup
 import re
 import requests
 import pandas as pd
+import random
+
+# Used to slice list after shuffling a list of several thousand names
+NUM_NAMES = 100
 
 # TODO: Should we make this search for exact names? Right now "Daniel" also returns "Danielle"
 BASE_QUERY_CS_URL = "https://directory.utexas.edu/index.php?q=%28%26%28cn%3D{0}*%29%28utexasEduPersonMajor%3D*Compute"\
@@ -22,21 +26,33 @@ def master_clean(raw_html):
     return [name.strip() for name in names]
 
 
+def shorten_list(raw_name_list):
+    random.shuffle(raw_name_list)
+    return raw_name_list[0:NUM_NAMES]
+
+
 name_list = []
 master_list = []
 
 # Create a list of names from text file
-with open('search_names.txt') as f:
+with open('cmu_male_names.txt') as f:
     for line in f:
         name_list.append(line.rstrip('\n'))
 
+with open('cmu_female_names.txt') as f:
+    for line in f:
+        name_list.append(line.rstrip('\n'))
+
+shortened_list = shorten_list(name_list)
+
 # TODO: @Stefan make this more Pythonic
-for individual_name in name_list:
+for individual_name in shortened_list:
     returned_names = make_query(individual_name)
 
     # TODO: add check for 'vCard', we also need to figure out what happens when no names are returned
     for single_name in returned_names:
-        master_list.append(single_name)
+        if len(single_name) > 0 and single_name != "vCard":
+            master_list.append(single_name)
 
 # Create a Pandas dataframe from master_list
 df = pd.DataFrame(master_list, columns=["Name"])
